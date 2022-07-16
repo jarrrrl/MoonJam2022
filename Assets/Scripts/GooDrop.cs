11d03c1b32e4b3f88a53e9e8dc21113abb9d7d69
Jared B <56705789+jarrrrl@users.noreply.github.com>
2022-07-16 01:04:56 -0600

    Goo drip FFX
    Added a small ffx when the goo hits something

--- Assets/Scripts/GooDrop.cs
+++ Assets/Scripts/GooDrop.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class GooDrop : MonoBehaviour
 {
+
+    public GameObject GooDropFFXPrefab;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(this.gameObject);
+        GameObject GooFFX = Instantiate(GooDropFFXPrefab, this.transform.position, Quaternion.identity);
+        Destroy(GooFFX, 1f);
     }
 }
