b01f7b54abbf0c977895f6905950a598d32768b2
Jared B <56705789+jarrrrl@users.noreply.github.com>
2022-07-15 23:19:31 -0600

    finished one basic enemy (the borpa)
    Made an enemy that goes back and forth and shoots a projectile when it reaches the end of a platform or hits something.
    
    Am going to make the projectile drip goo from where it lands

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void OnShoot(float rotation)
+    {
+        Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
+        float speed = 5f;
+        rb.AddForce(new Vector2(rotation, 0) * speed, ForceMode2D.Impulse);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!collision.tag.Equals("Enemy"))
+            Destroy(this.gameObject);
+    }
+}
