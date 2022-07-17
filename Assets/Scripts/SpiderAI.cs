3a7ff4d30ca82d94a36a23780e52471291c783ef
Jared B <56705789+jarrrrl@users.noreply.github.com>
2022-07-16 21:11:14 -0600

    changed spider ai logic
    spider now jumps periodically towards player upon activation (within range of player). The jump itself needs implementation and the part where it starts stuck to a ceiling (Maybe) needs implementation

--- Assets/Scripts/SpiderAI.cs
+++ Assets/Scripts/SpiderAI.cs
@@ -2,17 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpiderAI : MonoBehaviour
+public class SpiderAI : EnemyAI
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Rigidbody2D rb;
+    private bool isActive = false;
+    private Transform spiderTransform;
+    private Transform playerTransform;
+    public LayerMask layerMask;
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spiderTransform = this.transform;
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.DrawLine(spiderTransform.position, playerTransform.position);
+
+        RaycastHit2D isPlayerInRange = Physics2D.Linecast(spiderTransform.position, playerTransform.position, layerMask);
+        if (!isActive && isPlayerInRange.transform.tag.Equals("Player"))
+        {
+            StartCoroutine(Jump());
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player"))
+            Destroy(this.gameObject);
+    }
+
+    private IEnumerator Jump()
+    {
+        isActive = true;
+        while (true)
+        {
+            //jump here
+            yield return new WaitForSeconds(3f);
+        }
     }
 }
