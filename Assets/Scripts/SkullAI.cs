e5671e56d0217c66df326a2985299b56267af413
Jared B <56705789+jarrrrl@users.noreply.github.com>
2022-07-16 20:22:29 -0600

    spider enemy pathfinding
    Created a skull enemy that chases the player through obstacles, and a spider. The spider needs work on pathfinding and activation

--- Assets/Scripts/SkullAI.cs
+++ Assets/Scripts/SkullAI.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
-public class SkullAI : MonoBehaviour
+public class SkullAI : EnemyAI
 {
+
+    private AIDestinationSetter aiDestination;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        aiDestination = this.GetComponent<AIDestinationSetter>();
+        aiDestination.target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.tag.Equals("Player"))
+            Destroy(this.gameObject);
     }
 }
