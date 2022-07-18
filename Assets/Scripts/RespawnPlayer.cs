e6cbd645c6b632c64998d86c8e98847fcb23f03c
MerzlyakovSergey <ser2xp@yandex.ru>
2022-07-19 01:00:47 +0400

    123123
    123123

--- Assets/Scripts/RespawnPlayer.cs
+++ Assets/Scripts/RespawnPlayer.cs
@@ -6,9 +6,7 @@ using UnityEngine.SceneManagement;
 public class RespawnPlayer : MonoBehaviour
 {
     public LevelManager levelManager;
-    // Start is called before the first frame update
-    private void Awake()
-    {
+    private void Start(){
         levelManager = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManager>();
         transform.position = levelManager.lastCheckpointPos;
     }
